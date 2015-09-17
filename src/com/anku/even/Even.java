ed93688572257c237efaa87a8035a262cd19857c
Akanksha Pansare <akankshapansare@gmail.com>
2015-09-16 20:14:07 -0700

    Add Output description in Even.java

--- src/com/anku/even/Even.java
+++ src/com/anku/even/Even.java
@@ -3,6 +3,7 @@ package com.anku.even;
 class Even {
     public static void main(String[] args) {
         int i = 2;
+        System.out.println("Even numbers:");
         System.out.println(i);
         for (int a = 0; a < 10; a++) {
             i = i + 2;
